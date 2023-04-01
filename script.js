7900535afa64341f2aca9d00dc6463dd6b1a58e3
Sayantan Choudhury <127319225+Sch476@users.noreply.github.com>
2023-04-02 01:47:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Full Stack Developer","Gamer","CS Undergrad", "Programmer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
